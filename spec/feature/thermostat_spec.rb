5cc6bf179994285d7bd7e9e657a92b7dd0818207
byverdu <byverdu@gmail.com>
2014-09-19 13:31:19 +0100

    Before the user interaction tests passed

--- spec/feature/thermostat_spec.rb
+++ spec/feature/thermostat_spec.rb
@@ -4,5 +4,16 @@ feature "On the home page" do
 		visit '/'
 
 		expect(page).to have_content('Thermostat')
+
+		expect(page).to have_content('20')
+		
+		expect(page).to have_button('Celsius')
+		expect(page).to have_button('Fahrenheit')
+		
+		expect(page).to have_selector('input[value="Saving Mode"]')
+
+		expect(page).to have_content('↑')
+		expect(page).to have_content('↓')
+
 	end
 end
